d5f4ea16b19e2678ad7824f47f36e812e5b8b4f9
qianxiaobai <42053471+qianxiaobai@users.noreply.github.com>
2019-08-07 09:36:14 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'views/home/index.vue'
 import Mine from "views/mine/index.vue"
+import Collection from "./collection/index"
 Vue.use(Router)
 
 export default new Router({
@@ -17,38 +18,8 @@ export default new Router({
       path: '/mine',
       name: 'mine',
       component: Mine,
-      // children: [{
-      //     name: "order",
-      //     path: "order",
-      //     component: () => import("components/mine/order.vue"),
-      //   },
-      //   {
-      //     name: "accbalance",
-      //     path: "accbalance",
-      //     component: () => import("components/mine/accbalance.vue"),
-      //   },
-      //   {
-      //     name: "car",
-      //     path: "car",
-      //     component: () => import("components/mine/car.vue"),
-      //   },
-      //   {
-      //     name: "comment",
-      //     path: "comment",
-      //     component: () => import("components/mine/comment.vue"),
-      //   },
-      //   {
-      //     name: "credit",
-      //     path: "credit",
-      //     component: () => import("components/mine/credit.vue"),
-      //   },
-      //   {
-      //     name: "redpack",
-      //     path: "redpack",
-      //     component: () => import("components/mine/redpack"),
-      //   }
-      // ]
     },
+    Collection,
     {
       path:"/cityList",
       component:()=>import("views/cityList"),
