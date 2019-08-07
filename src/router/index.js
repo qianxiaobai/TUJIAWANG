cb8a5cee7fedeeab62f9a27048e68737d0896dd5
qianxiaobai <1798895401@qq.com>
2019-08-07 09:40:36 +0800

    收藏4

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import Router from 'vue-router'
 import Home from 'views/home/index.vue'
 import Mine from "views/mine/index.vue"
 import Collection from "./collection/index"
-
 Vue.use(Router)
 
 export default new Router({
@@ -40,44 +39,9 @@ export default new Router({
         component:()=>import("components/foreign"),
       }]
     }
-
+    
   ]
-
 })
 
-      // children: [{
-      //     name: "order",
-      //     path: "order",
-      //     component: () => import("components/mine/order.vue"),
-      //   },
-      //   {
-      //     name: "accbalance",
-      //     path: "accbalance",
-      //     component: () => import("components/mine/accbalance.vue"),
-      //   },
-      //   {
-      //     name: "car",
-      //     path: "car",
-      //     component: () => import("components/mine/car.vue"),
-      //   },
-      //   {
-      //     name: "comment",
-      //     path: "comment",
-      //     component: () => import("components/mine/comment.vue"),
-      //   },
-      //   {
-      //     name: "credit",
-      //     path: "credit",
-      //     component: () => import("components/mine/credit.vue"),
-      //   },
-      //   {
-      //     name: "redpack",
-      //     path: "redpack",
-      //     component: () => import("components/mine/redpack"),
-      //   }
-      // ]
-
-            // ]
-   // ]
 
    
